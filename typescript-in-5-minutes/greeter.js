a273423040d74b93baefb0f399e3372440068392
Yoko TAMADA <tamada@feedforce.jp>
2017-08-09 10:49:07 +0900

    Try type annotations

--- typescript-in-5-minutes/greeter.js
+++ typescript-in-5-minutes/greeter.js
@@ -1,5 +1,9 @@
+// TypeScript in 5 minutes
+// https://www.typescriptlang.org/docs/handbook/typescript-in-5-minutes.html
 function greeter(person) {
     return "Hello, " + person;
 }
-var user = "Toko YAMADA";
+var user = "Yoko TAMADA";
+// typescript-in-5-minutes/greeter.ts(11,35): error TS2345: Argument of type 'number[]' is not assignable to parameter of type 'string'.
+// var user = [0, 1, 2];
 document.body.innerHTML = greeter(user);
--- typescript-in-5-minutes/greeter.ts
+++ typescript-in-5-minutes/greeter.ts
@@ -1,7 +1,11 @@
-function greeter(person) {
+// TypeScript in 5 minutes
+// https://www.typescriptlang.org/docs/handbook/typescript-in-5-minutes.html
+function greeter(person: string) {
   return "Hello, " + person;
 }
 
-var user = "Toko YAMADA";
+var user = "Yoko TAMADA";
+// typescript-in-5-minutes/greeter.ts(11,35): error TS2345: Argument of type 'number[]' is not assignable to parameter of type 'string'.
+// var user = [0, 1, 2];
 
 document.body.innerHTML = greeter(user);
